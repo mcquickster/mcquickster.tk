abda47e63602b8e5b555f7079c417e519efdecdb
mcquickster <lukescoop2@gmail.com>
2021-02-14 15:10:23 -0500

    commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function GreetFunction() {
     var Name1 = document.getElementById("Name");
     var Name2 = Name1.value;
-    alert("Hi " + Name2);
+    alert("Hi " + Name2 + "! Welcome to mcquickster.tk");
     }
