6a52034dc887362ebf9f88e13449ef3855312f7d
mcquickster <lukescoop2@gmail.com>
2021-02-14 15:57:03 -0500

    commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function GreetFunction() {
     var Name1 = document.getElementById("Name");
     var Name2 = Name1.value;
-    if(Name2.value == ""){
+    if(Name1.value == ""){
         alert("Pleas enter your name, you bum!");
     }
     else {
